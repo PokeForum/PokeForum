9531b745ce8ba0bd259d1b32fe17874f1e3f0dc9
Nuanxinqing <nuanxinqing@gmail.com>
2025-11-20 15:53:22 +0800

    feat: adjust CORS middleware policy

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -7,14 +7,13 @@ import (
 )
 
 var AllowOrigins = []string{
-	"http://127.0.0.1:3000",
-	"http://localhost:3000",
+	"*",
 }
 
 var CorsConfig = cors.Config{
-	AllowAllOrigins:  false,
+	AllowAllOrigins:  true,
 	AllowOrigins:     AllowOrigins,                                                          // 允许的源，生产环境中应替换为具体的允许域名
-	AllowOriginFunc:  func(origin string) bool { return true },                              // 自定义函数来判断源是否允许
+	AllowOriginFunc:  nil,                                                                   // 自定义函数来判断源是否允许
 	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},  // 允许的HTTP方法列表
 	AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"}, // 允许的HTTP头部列表
 	AllowCredentials: true,                                                                  // 是否允许浏览器发送Cookie
