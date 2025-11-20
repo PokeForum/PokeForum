b7cdfa93524f1b9fd88e9353d3dd351e948faa09
Nuanxinqing <nuanxinqing@gmail.com>
2025-11-20 15:55:59 +0800

    chore: 更新 CORS 中间件配置

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -6,14 +6,8 @@ import (
 	"github.com/gin-contrib/cors"
 )
 
-var AllowOrigins = []string{
-	"*",
-}
-
 var CorsConfig = cors.Config{
 	AllowAllOrigins:  true,
-	AllowOrigins:     AllowOrigins,                                                          // 允许的源，生产环境中应替换为具体的允许域名
-	AllowOriginFunc:  nil,                                                                   // 自定义函数来判断源是否允许
 	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},  // 允许的HTTP方法列表
 	AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"}, // 允许的HTTP头部列表
 	AllowCredentials: true,                                                                  // 是否允许浏览器发送Cookie
